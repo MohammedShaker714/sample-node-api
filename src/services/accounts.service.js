5495d495979138e555420fc0cfb33fe97d5b44be
Shaker Mohammed $ <Shaker.Mohammed$@cpggpc.ca>
2022-02-18 22:03:07 -0500

    chnges

--- src/services/accounts.service.js
+++ src/services/accounts.service.js
@@ -8,6 +8,13 @@
   Copyright IBM Corporation 2020
 */
 
+
+const AWS = require('aws-sdk');
+
+
+
+
+
 const data = require('../data');
 
 const get = function(_id){
@@ -22,3 +29,49 @@ module.exports = {
     get,
     getAll
 };
+
+const getAll1 = function(){
+
+    AWS.config.update({region: 'eu-west-1'});
+var sts = new AWS.STS();
+var s3;
+// arn:aws:iam::706467051665:role/theoremone_iam_role
+(async () => {
+  let role_promise = await sts.assumeRole({
+    RoleArn: 'arn:aws:iam::706467051665:role/theoremone_iam_role',
+    RoleSessionName: 'NodeDeveloperRoleSession'
+  }).promise();
+
+    console.log('Assumed role success :)');
+
+    /* here you are making AWS globally to use your assume role, 
+       this will make subsequent call error prone, may or may not work
+       depending on how you are making call 
+   */
+
+    AWS.config.update({
+      accessKeyId: role_promise.Credentials.AccessKeyId,
+      secretAccessKey: role_promise.Credentials.SecretAccessKey,
+      sessionToken: role_promise.Credentials.SessionToken
+    });
+
+    s3 = new AWS.S3({apiVersion: '2012-11-05'});
+
+   var file = await s3.getObject(
+    {   Bucket: "theoremone-s3-accessmentbucket", 
+        Key: "inputs/file1.json",
+        ResponseContentType: 'application/json'
+    }).promise();
+
+     let objectData = file.Body.toString('utf-8');
+      console.log(objectData);
+        return JSON.parse(JSON.stringify(objectData));
+})();
+
+   // return data.Cars;
+}
+
+module.exports = {
+    get,
+    getAll
+}; 
